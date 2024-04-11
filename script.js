1c852252465ce39b860e62d009b4b95dc2b01ab3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-11 20:18:21 -0300

    java script kekw

---
+++ script.js
@@ -0,0 +1,94 @@
+// AQUI EXIBE UMA MENSAGEM NO CONSOLE
+
+console.log("Direto do arquivo externo");
+
+//alert("CUIDADO!!! SEU PC PODE ESTAR EM PERIGO!!!");
+
+const PI = 3.14;
+
+console.log (PI)
+
+//LEGADO (NÃO RECOMENDADO)
+var nome = "ezno games";
+console.log (nome);
+
+// MANEIRA CORRETA
+let sobrenome = "Miguel";
+console.log(sobrenome);
+
+//CONTINHA
+let n1 = 2;
+let n2 = 6;
+console.log(n1+n2);
+
+n2 = "Agora é texto";
+console.log(n2);
+
+//BOOLEAO
+let bool = true;
+console.log(bool);
+bool = false;
+console.log(bool); 
+ 
+//COMPARAÇÃO
+console.log(bool === "0");
+
+// OPERADORES LÓGICOS
+let var01 = true;
+let var02 = true;
+
+
+// AND
+console.log(var01 && var02);
+
+// OR
+console.log(var01 || var02);
+
+//NOT
+console.log(!var01);
+
+//undefined
+let sem_definir;
+console.log(sem_definir);
+
+
+//NULL
+let variavelNula = null;
+
+console.log(sem_definir == variavelNula);
+console.log ("" == variavelNula);
+
+//string
+let string = "Texto";
+let string2 = String("Texto");
+
+//POTENCIAÇÃO 
+let x = 2;
+console.log(x * x);
+console.log(x ** 2);
+
+//Resto
+let a = 3;
+let b = 2;
+
+console.log (a / b);
+console.log (a % b);
+
+
+//DIFERENÇA
+console.log(10 != 9);
+
+
+//Maior
+console.log(10 > 3);
+
+
+//Menor
+console.log(10 < 3);
+
+//Comparação de strings
+console.log("A" > "B");
+console.log("B" > "A");''
+
+
+
